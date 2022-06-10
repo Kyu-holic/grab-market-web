b38560298faa939f84cf0816831aff642aa30c58
Kyu-holic <76858933+Kyu-holic@users.noreply.github.com>
2022-06-11 00:53:36 +0900

    Add files via upload

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,23 @@
 import "./style.css";
+import axios from "axios";
+import react, { useState, useEffect } from "react";
 
 function MainPage() {
+  const [products, setProducts] = useState([]);
+  useEffect(function () {
+    axios
+      .get(
+        "https://c67feb93-fe43-4341-a3b0-aebc0da65a0e.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생:", error);
+      });
+  }, []);
+
   return (
     <div>
       <div id="header">
@@ -13,7 +30,28 @@ function MainPage() {
           <img src="./images/banners/banner1.png" alt="" />
         </div>
         <h1>판매되는 상품들</h1>
-        <div id="product-list"></div>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="products-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div id="footer"></div>
     </div>
